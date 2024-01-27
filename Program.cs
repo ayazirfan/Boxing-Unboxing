2b545080f907428d2a4d3ced67d6b6edf8e73e9c
ayazirfan <byirfanayaz@gmail.com>
2024-01-27 14:22:51 +0300

    BTK Akademi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i=23;
+object o =i;
+
+// Unboxing- Kutudan çıkarma
+i*=2;
+i= (int)o;
+System.Console.WriteLine("Değer turu: {0}", i);
+System.Console.WriteLine("Referans turu: {0}",o);
